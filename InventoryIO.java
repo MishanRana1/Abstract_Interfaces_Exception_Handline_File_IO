d4db1ad5e49618f51e909fd1ea2dd1b5abdcd525
MishanRana1 <mishanrana12@gmail.com>
2025-10-08 16:52:24 +0000

    InventoryIO class done.

---
+++ InventoryIO.java
@@ -0,0 +1,21 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Collection;
+
+class InventoryIO {
+    public static void saveEnergyReport(String path, Collection<? extends MeasurableEnergy> items)
+            throws IOException {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(path))) {
+            bw.write("Energy Report (daily estimates)\n");
+            bw.write("================================\n");
+            for (MeasurableEnergy m : items) {
+                String line = m.getClass().getSimpleName() +
+                        " -> " + String.format(Locale.US, "%.2f kWh/day", m.kWhPerDay());
+                bw.write(line);
+                bw.newLine();
+            }
+        }
+    }
+}
